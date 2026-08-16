6dc55d8e1652ce2ee8154a1a5f0cb0b3e93a3e3d
Aaron <aaronltilley1@gmail.com>
2026-08-16 18:00:28 -0500

    fix for import

--- my-personal-site/@/components/ui/navigation-menu.tsx
+++ my-personal-site/@/components/ui/navigation-menu.tsx
@@ -1,7 +1,7 @@
 import { NavigationMenu as NavigationMenuPrimitive } from "@base-ui/react/navigation-menu"
 import { cva } from "class-variance-authority"
 
-import { cn } from "@/lib/utils"
+import { cn } from "../../lib/utils"
 import { HugeiconsIcon } from "@hugeicons/react"
 import { ArrowDown01Icon } from "@hugeicons/core-free-icons"
 
