4e305a179e673ee246ca927d26600dd5d776977a
Aaron <aaronltilley1@gmail.com>
2026-09-13 07:45:24 -0500

    update the want to play list

--- my-personal-site/src/components/games/WantToPlay.tsx
+++ my-personal-site/src/components/games/WantToPlay.tsx
@@ -1,6 +1,6 @@
 import { headingClass } from './shared'
 
-const wantToPlay = ['Frosthaven', 'Ark Nova']
+const wantToPlay = ['Ark Nova', 'Perseverance: Cast Away Chronicles Ep. 1 & 2', 'The Crew: Mission Deep Sea', 'The Lord of the Rings: Fellowship of the Ring - Trick-Taking Game ']
 
 export function WantToPlay() {
   return (
